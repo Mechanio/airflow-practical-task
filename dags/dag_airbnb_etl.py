72223aad4cf16cb669620c6354117e99b678fc7d
mechanio <mechanio5522@gmail.com>
2024-09-04 21:50:36 +0300

    DAG modification

--- dags/dag_airbnb_etl.py
+++ dags/dag_airbnb_etl.py
@@ -1,8 +1,13 @@
+# NOT FULLY DONE
+# relative path error
 from datetime import datetime, timedelta
 import os
 from airflow import DAG
 import pandas as pd
 from airflow.operators.python import PythonOperator
+from airflow.providers.postgres.operators.postgres import PostgresOperator
+from airflow.providers.postgres.hooks.postgres import PostgresHook
+
 
 default_args = {
     'owner': 'mechanio',
@@ -16,19 +21,32 @@ TRANSFORMED_PATH = "../transformed/transformed_data.csv"
 def get_data():
     if os.path.exists(RAW_PATH):
         df = pd.read_csv(RAW_PATH)
-        df.to_csv(TRANSFORMED_PATH, index=False)
     else:
         raise FileNotFoundError(f"{RAW_PATH} does not exist.")
 
 
 def transform_data():
-    df = pd.read_csv(TRANSFORMED_PATH)
+    df = pd.read_csv(RAW_PATH)
     df = df[df['price'] > 0]
     df['last_review'] = pd.to_datetime(df['last_review'])
     df['last_review'].fillna(df['last_review'].min(), inplace=True)
     df.fillna(value={'name': 'Unknown', 'host_name': 'Unknown', 'reviews_per_month': 0}, inplace=True)  # Fill missing values
     df.dropna(subset=['latitude', 'longitude'], inplace=True)
     df.to_csv(TRANSFORMED_PATH, index=False)
+
+
+def data_quality_check():
+    transformed_df = pd.read_csv(TRANSFORMED_PATH)
+    expected_count = len(transformed_df)
+
+    hook = PostgresHook(postgres_conn_id='postgres_localhost')
+    records = hook.get_records("SELECT COUNT(*) FROM airbnb_listings;")
+    record_count = records[0][0]
+
+    if record_count != expected_count:
+        raise ValueError(f"Data quality check failed: Expected {expected_count} records, but found {record_count} records.")
+    else:
+        print(f"Data quality check passed: Found {record_count} records.")
 
 
 with DAG(
@@ -45,5 +63,20 @@ with DAG(
         task_id='transform_data',
         python_callable=transform_data,
     )
+    load_data_to_postgres = PostgresOperator(
+        task_id='load_data_to_postgres',
+        postgres_conn_id='postgres_localhost',
+        sql="""
+        COPY airbnb_listings(name, host_id, host_name, neighbourhood_group, neighbourhood, latitude, longitude, room_type, price, minimum_nights, number_of_reviews, last_review, reviews_per_month, calculated_host_listings_count, availability_365)
+        FROM '{{ params.transformed_file_path }}'
+        DELIMITER ','
+        CSV HEADER;
+        """,
+        params={'transformed_file_path': TRANSFORMED_PATH}
+    )
+    quality_check = PythonOperator(
+        task_id='data_quality_check',
+        python_callable=data_quality_check
+    )
 
-    extract_data >> transform_data
+    extract_data >> transform_data >> load_data_to_postgres >> quality_check
